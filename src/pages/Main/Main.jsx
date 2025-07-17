c4344197ce658f109b1c0a353d9887d9c05d244e
kenoleeee <tatherroyale@gmail.com>
2025-07-18 01:19:31 +0300

    Add loading state and skeleton components to Main page

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,11 +1,14 @@
 import styles from './styles.module.css';
 import NewsBanner from '../../components/NewsBanner/NewsBanner';
 import NewsList from '../../components/NewsList/NewsList';
+import Skeleton from '../../components/Skeleton/Skeleton';
+
 import { getNews } from '../../api/apiNews';
 import { useEffect, useState } from 'react';
 
 const Main = () => {
     const [news, setNews] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchNews = async () => {
@@ -15,6 +18,8 @@ const Main = () => {
             }
             catch (error) {
                 console.error('Error fetching news:', error);
+            } finally {
+                setIsLoading(false);
             }
         }
         fetchNews();
@@ -22,11 +27,11 @@ const Main = () => {
 
     return (
         <>
-
             <main className={styles.main}>
-                {news.length > 0 ? <NewsBanner item={news[0]} /> : null}
 
-                <NewsList news={news} />
+                {news.length > 0 && !isLoading ? (<NewsBanner item={news[0]} />) : (<Skeleton count={1} type="banner" />)}
+
+                {!isLoading ? (<NewsList news={news} />) : (<Skeleton count={10} type="item" />)}
             </main>
         </>
     )
